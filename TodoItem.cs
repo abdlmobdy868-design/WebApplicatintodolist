909141f1ee31d5cd3e8ebdfbd82b09ea2d8653f7
abdlmobdy868-design <abdlmobdy868@gmail.com>
2026-08-08 21:09:13 +0300

    model

---
+++ TodoItem.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+public class TodoItem
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Title is required")]
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    [DataType(DataType.DateTime)]
+    public DateTime Deadline { get; set; } = DateTime.Now.AddDays(1);
+
+    public string? FilePath { get; set; } // مسار الملف
+}

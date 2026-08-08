362dbfb62616e8b92d19269ca74b144369f5f2e0
abdlmobdy868-design <abdlmobdy868@gmail.com>
2026-08-08 21:10:43 +0300

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationtodolist.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplicationtodolist.Data
+{
+    
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+            public DbSet<TodoItem> TodoItems { get; set; }
+        }
+    
+}
